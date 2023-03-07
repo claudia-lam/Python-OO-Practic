6efade3f1da2a2199a65c2fb9f2df49945ee6296
Claudia Lam <claudiaslam@gmail.com>
2023-03-07 15:08:45 -0800

    add solution to serial.py

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,24 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """
+        create serial generator beginning at start
+        """
+        self.start = start
+        self.counter = 0
+
+    def generate(self):
+        """
+        return a new sequential number
+        """
+        current_counter = self.counter
+        self.counter += 1
+        return self.start + current_counter
+
+    def reset(self):
+        """
+        reset the current number back to 0
+        """
+        self.counter = 0
